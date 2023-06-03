0311d020abd6465a08f991c479f7f13a10250f51
camellia14 <adapter1@hotmail.co.jp>
2023-06-03 17:22:03 +0900

    Settingsを実装

--- LoadTestTool/Program.cs
+++ LoadTestTool/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LoadTestTool;
+using System.Collections;
+using System.ComponentModel;
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
 
 string res = await LoadTestTool.Web.Get("https://www.bing.com/");
+
+var settings = new LoadTestTool.Settings(args);
 
 Console.WriteLine(res);
---
+++ LoadTestTool/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadTestTool
+{
+    internal class Settings
+    {
+        public Settings() { }
+        public Settings(string[] args) { LoadArguments(args); }
+        public void LoadArguments(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                var values = arg.Split('=');
+                if (values.Length != 2) continue;
+                switch (values[0])
+                {
+                    case "count":
+                        bot_count = int.Parse(values[1]);
+                        break;
+                    case "scenario":
+                        scenario_func_name = values[1];
+                        break;
+                    case "file":
+                        scenario_func_name = "ScenarioFromFile";
+                        scenario_file_name = values[1];
+                        break;
+                }
+            }
+        }
+        public int bot_count = 1;
+        public String scenario_func_name = "";
+        public String scenario_file_name = "";
+    }
+}
